faf2cdfcf17c0177559b56bedd903f13bb73a7c5
ANDRES <andresarias328826@correo.itm.edu.co>
2025-11-26 10:30:04 -0500

    Creacion de dtos y mappers

--- API.Movies/CAPA_DE_ACCESSO_DATOS/MODELS/Dtos/CategoryDto.cs
+++ API.Movies/CAPA_DE_ACCESSO_DATOS/MODELS/Dtos/CategoryDto.cs
@@ -8,7 +8,7 @@ namespace API.Movies.CAPA_DE_ACCESSO_DATOS.MODELS.Dtos
         [Required(ErrorMessage = "El nombre de la categoria es obligatorio. ")]
         [MaxLength(100, ErrorMessage = "El número máximo de caracteres es 100. ")]
         public string Name { get; set; }
-        public DateTime CreatedDate { get; set; }   
+        public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
 }
